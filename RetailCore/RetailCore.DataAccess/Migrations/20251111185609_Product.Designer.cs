552f9dcee0a68edc546d7c19d36d5a249c8f04b2
subashcse009-bit <subashcse009@gmail.com>
2025-11-12 00:27:15 +0530

    Product Model

--- RetailCore/RetailCore.DataAccess/Migrations/20251111185609_Product.Designer.cs
+++ RetailCore/RetailCore.DataAccess/Migrations/20251111185609_Product.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RetailCore.DataAccess.Data;
 
@@ -10,9 +11,11 @@ using RetailCore.DataAccess.Data;
 namespace RetailCore.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20251111185609_Product")]
+    partial class Product
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RetailCore/RetailCore.Model/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailCore.Model
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required] 
+        public string Description { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Display(Name = "Display Price")]
+        [Range(1, 10000)] 
+        public double ListPrice { get; set; }
+        [Display(Name = "Price for 1-50")]
+        [Range(1, 10000)]
+        public double Price { get; set; }
+        [Display(Name = "Price for 50+")]
+        [Range(1, 10000)]
+        public double Price50 { get; set; }
+        [Display(Name = "Price for 100+")]
+        [Range(1, 10000)]
+        public double Price100 { get; set; }
+    }
+}
